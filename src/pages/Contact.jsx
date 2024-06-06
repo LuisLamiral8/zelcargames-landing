6421ad1e11092d87c2d3c996438e7834f9ca913e
Luis <67067808+LuisLamiral8@users.noreply.github.com>
2024-06-06 16:08:07 -0300

    Contact page finishied, desktop mobile

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,31 @@ const Contact = () => {
   return (
     <Layout>
       <main className={styles.container}>
-        <h1>Contact</h1>
+        <img
+          className={styles.overlayUp}
+          src="/contact/overlay-up.png"
+          alt=""
+        />
+        <img
+          className={styles.overlayDown}
+          src="/contact/overlay-down.png"
+          alt=""
+        />
+        <img src="/contact/alufi.png" className={styles.alufi} alt="" />
+        <div className={styles.main}>
+          <div className={styles.separator}></div>
+          <h3>Contact us</h3>
+          <input type="text" placeholder="your@email.com" />
+          {/* <input type="textarea" placeholder="How can we help you?" /> */}
+          <textarea placeholder="How can we help you?"></textarea>
+          <button>Send</button>
+          <div className={styles.info}>
+            <p>Or contact us through the following emails:</p>
+            <p>ceo@secretforest.world</p>
+            <p>c@zelcar.games</p>
+          </div>
+          <div className={styles.separator}></div>
+        </div>
       </main>
     </Layout>
   );
